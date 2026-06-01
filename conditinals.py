2507718c9989cffc5e8afb3e8267e82549a0dcd1
apurvaloopwork-gif <apurva.loopwork@gmail.com>
2026-06-01 14:09:17 +0530

    Added conditionals

---
+++ conditinals.py
@@ -0,0 +1,68 @@
+#print(4 != 5)
+#print(2>3)
+
+# print('hello'=='helo')
+
+# print('hello'!='helo')
+#print('Hello'=='hello') In python Capital letter and lower case letter is different
+
+# print(2-3+4 > 5)
+# print(False==True) 
+
+
+
+#IF, ELSE , ELSEIF
+
+
+# age = input("Enter your age:")
+
+
+# if int(age) >= 18:
+#   print("You are older than 18!!!")
+# else:
+#   print("you are younger than 18")
+
+
+#check if you are over 18
+
+# age = input("Enter your age:")
+
+# if int(age) > 18:
+#   print("You are over 18 !")
+# elif int(age) == 18:
+#   print("You are  18 !")
+# else:
+#   print("You are not old enough!")
+
+
+# x = 2
+# y = 3
+# if x == y or x < y :
+#   print('ran')
+
+# x = 2
+# y = 3
+# if x == y and x < y :
+#   print('ran')
+
+# x = 2
+# y = 3
+# if not x == y and x < y :
+#   print('ran')
+
+
+
+#Nested statements 
+
+x = 3
+y = 2
+
+if x == 3:
+   if y==2:
+     print('X=3 , y=2')
+   else:
+     print('X=3, Y!=2')
+else:
+  print(x != 2)
+
+
