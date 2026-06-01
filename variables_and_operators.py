3fc3db8a46db26135cbf8a91f96588ee2f724d8a
Apurva <sapurva23cs@stdent.mes.ac.in>
2026-06-01 11:08:44 +0530

    Intial commit

---
+++ variables_and_operators.py
@@ -0,0 +1,27 @@
+# print("What is your name ?")
+# name=input()
+# print("Hello!!!",name)
+
+
+# print('Pick one number')
+# num1 =int(input())
+
+# print('Pick one number')
+# num2 =int(input())
+
+# sum=num1+num2
+# print(sum)
+
+
+# print('Pick one number')
+# num1 =int(input())
+
+# print('Pick one number')
+# num2 =int(input())
+
+# mult=num1*num2
+# print(mult)
+
+
+
+
