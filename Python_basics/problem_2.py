8afae989cf12782898deda6a81a27d83d005967c
apurvaloopwork-gif <apurva.loopwork@gmail.com>
2026-06-11 12:53:59 +0530

    corrected code

--- Python_basics/problem_2.py
+++ Python_basics/problem_2.py
@@ -18,7 +18,7 @@
 def calculate_emi(principal,annual_rate,months):
   monthly_rate=annual_rate /12 /100
   Emi = principal * monthly_rate *(1+monthly_rate)**months / ((1+monthly_rate)**months-1)
-  return roun1d(Emi)
+  return round(Emi,2)
 
 pr = int(input("Enter the principle:"))
 
